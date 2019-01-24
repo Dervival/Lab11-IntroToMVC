c7ca9a4b3fad68c300f21d8e573a474cb22ea753
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-23 18:53:57 -0800

    Added basic model for TOTY people and a way to serialize them from csv, going to try to render to page now

---
+++ TimePersonOfTheYear/TimePersonOfTheYear/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TimePersonOfTheYear.Models;
+
+namespace TimePersonOfTheYear.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../wwwroot/personOfTheYear.csv");
+            string[] allPeople = System.IO.File.ReadAllLines(path);
+            List<TimePerson> allTimePeople = ConvertCSVToTimePersonList(allPeople);
+            //allPeople = System.IO.File.ReadAllLines("../../../../../personOfTheYear.csv");
+            return View(allTimePeople);
+        }
+
+        public List<TimePerson> ConvertCSVToTimePersonList(string[] csv)
+        {
+            List<TimePerson> allTimePeople = new List<TimePerson>();
+            for(int i = 1; i < csv.Length; i++)
+            {
+                TimePerson timePerson = new TimePerson();
+                string[] splitCsv = csv[i].Split(',');
+                timePerson.Year = Convert.ToInt32(splitCsv[0]);
+                timePerson.Honor = splitCsv[1];
+                timePerson.Name = splitCsv[2];
+                timePerson.Country = splitCsv[3];
+                timePerson.BirthYear = Convert.ToInt32(splitCsv[4]);
+                timePerson.DeathYear = Convert.ToInt32(splitCsv[5]);
+                timePerson.Title = splitCsv[6];
+                timePerson.Category = splitCsv[7];
+                timePerson.Context = splitCsv[8];
+                allTimePeople.Add(timePerson);
+            }
+            return allTimePeople;
+        }
+    }
+}
